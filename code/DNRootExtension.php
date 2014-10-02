2a9c032189a50aa2f7754f914ebf85b147318b17
Stephen Shkardoon <ss23@ss23.geek.nz>
2014-10-02 22:24:47 +1300

    Update module to work with the actually implemented extension point
    Also correctly sets the module-type in composer now too

--- code/DNRootExtension.php
+++ code/DNRootExtension.php
@@ -2,12 +2,16 @@
 
 class DNRootExtension extends Extension {
 
-	public function DoDeploy($project, $environment, $selectRelease, $buildName) {
+	public function doDeploy($project, $environment, $buildName, $data) {
+		// We only want to log user initiated deployments
+		if (empty($data['SelectRelease'])) {
+			return;
+		}
 		$stat = new DeploymentStat();
 		$stat->ProjectID = $project->ID;
 		$stat->EnvironmentID = $environment->ID;
 		$stat->DeployerID = Member::currentUserID();
-		$stat->ReleaseType = $selectRelease;
+		$stat->ReleaseType = $data['SelectRelease'];
 
 		$stat->write();
 	}
